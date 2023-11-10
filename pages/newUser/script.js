f02962310cb2464a0fd48d976ca3e7015665da69
Yago <yagosr25@gmail.com>
2023-11-10 03:29:39 -0300

    first commit

---
+++ pages/newUser/script.js
@@ -0,0 +1,29 @@
+const input = document.querySelector('#cpf');
+input.addEventListener('keypress', () => {
+   let inputLength = input.value.length;
+
+   if (inputLength === 3 || inputLength === 7) {
+        input.value += '.';
+   } else if (inputLength === 11) {
+        input.value += '-';
+   }
+});
+
+function Rg(v){
+    v=v.replace(/\D/g,"");
+    v=v.replace(/(\d{2})(\d{3})(\d{3})(\d{1})$/,"$1.$2.$3-$4");
+    return v;
+}
+
+const handlePhone = (event) => {
+    let input = event.target
+    input.value = phoneMask(input.value)
+  }
+  
+  const phoneMask = (value) => {
+    if (!value) return ""
+    value = value.replace(/\D/g,'')
+    value = value.replace(/(\d{2})(\d)/,"($1) $2")
+    value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+    return value
+  }
